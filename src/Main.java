6385aeec7b655bc7bf451fa2118903175ac7b305
Naramkil <naramakil@gmail.com>
2024-09-23 19:24:27 +0300

    Add CamelCase

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance = 100; // Объявляем переменную и текущий баланс счета
-        int rub_per_bonus = 100; // Объявляем переменную и количество рублей за бонус
-        int paid_amount = 1100; // Объявляем переменную и указазываем сумму пополнения
+        int currentBalance = 100; // Объявляем переменную и текущий баланс счета
+        int rubPerBonus = 100; // Объявляем переменную и количество рублей за бонус
+        int paidAmount = 1100; // Объявляем переменную и указазываем сумму пополнения
         int bonus; // Объявляем переменную начисляемых бонусов
-        int total_balance; // Объявляем переменную общего баланса
-        if (paid_amount > 1000) {
-            bonus = paid_amount / rub_per_bonus;
-            total_balance = current_balance + paid_amount + bonus;
+        int totalBalance; // Объявляем переменную общего баланса
+        if (paidAmount > 1000) {
+            bonus = paidAmount / rubPerBonus;
+            totalBalance = currentBalance + paidAmount + bonus;
         } else {
             bonus = 0;
-            total_balance = current_balance + paid_amount;
+            totalBalance = currentBalance + paidAmount;
         }
         System.out.println("Бонусные баллы: " + bonus); // Выводим баланс бонусов
-        System.out.println("Баланс счета: " + total_balance); // Выводим общий баланс
+        System.out.println("Баланс счета: " + totalBalance); // Выводим общий баланс
     }
 }
